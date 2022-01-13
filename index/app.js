2f6a5dac28b93ba885aeedaf22c9a5f977c380f3
Varun <tvarun35@gmail.com>
2022-01-13 18:51:54 +0530

    Gridster Implemented

--- index/app.js
+++ index/app.js
@@ -1,6 +1,6 @@
-var app = angular.module("app", ["ui.router"]);
-window.baseUrl =
-  "https://c8ab-2401-4900-46aa-2a71-c57a-e855-53bb-21bc.ngrok.io";
+var app = angular.module("app", ["ui.router", "gridster"]);
+// var grid = angular.module("grid", ["gridster"]);
+window.baseUrl = "https://10.21.96.128:8000";
 
 app.service("user", function () {
   var username;
@@ -118,6 +118,7 @@ app.controller("dashboardCtrl", function ($scope, $http, user) {
 
   $scope.view = function () {
     $http({
+      //url: window.baseUrl + "/keepsapp/viewnotes",
       url: window.baseUrl + "/keepsapp/viewnotes",
       method: "GET",
       withCredentials: true,
@@ -140,6 +141,64 @@ app.controller("dashboardCtrl", function ($scope, $http, user) {
     }).then(function (response) {
       $scope.view();
     });
+  };
+  $scope.standardItems = [
+    { sizeX: 2, sizeY: 1, row: 0, col: 0 },
+    { sizeX: 2, sizeY: 2, row: 0, col: 2 },
+    { sizeX: 1, sizeY: 1, row: 0, col: 4 },
+    { sizeX: 1, sizeY: 1, row: 0, col: 5 },
+    { sizeX: 2, sizeY: 1, row: 1, col: 0 },
+    { sizeX: 1, sizeY: 1, row: 1, col: 4 },
+    { sizeX: 1, sizeY: 2, row: 1, col: 5 },
+    { sizeX: 1, sizeY: 1, row: 2, col: 0 },
+    { sizeX: 2, sizeY: 1, row: 2, col: 1 },
+    { sizeX: 1, sizeY: 1, row: 2, col: 3 },
+    { sizeX: 1, sizeY: 1, row: 2, col: 4 },
+  ];
+
+  $scope.gridsterOpts = {
+    columns: 6, // the width of the grid, in columns
+    pushing: true, // whether to push other items out of the way on move or resize
+    floating: true, // whether to automatically float items up so they stack (you can temporarily disable if you are adding unsorted items with ng-repeat)
+    swapping: false, // whether or not to have items of the same size switch places instead of pushing down if they are the same size
+    width: "auto", // can be an integer or 'auto'. 'auto' scales gridster to be the full width of its containing element
+    colWidth: "auto", // can be an integer or 'auto'.  'auto' uses the pixel width of the element divided by 'columns'
+    rowHeight: "match", // can be an integer or 'match'.  Match uses the colWidth, giving you square widgets.
+    margins: [10, 50], // the pixel distance between each widget
+    outerMargin: true, // whether margins apply to outer edges of the grid
+    sparse: false, // "true" can increase performance of dragging and resizing for big grid (e.g. 20x50)
+    isMobile: false, // stacks the grid items if true
+    mobileBreakPoint: 600, // if the screen is not wider that this, remove the grid layout and stack the items
+    mobileModeEnabled: true, // whether or not to toggle mobile mode when screen width is less than mobileBreakPoint
+    minColumns: 1, // the minimum columns the grid must have
+    minRows: 2, // the minimum height of the grid, in rows
+    maxRows: 100,
+    defaultSizeX: 2, // the default width of a gridster item, if not specifed
+    defaultSizeY: 1, // the default height of a gridster item, if not specified
+    minSizeX: 1, // minimum column width of an item
+    maxSizeX: null, // maximum column width of an item
+    minSizeY: 1, // minumum row height of an item
+    maxSizeY: null, // maximum row height of an item
+    resizable: {
+      // enabled: true,
+      handles: ["n", "e", "s", "w", "ne", "se", "sw", "nw"],
+      start: function (event, $element, widget) {}, // optional callback fired when resize is started,
+      resize: function (event, $element, widget) {}, // optional callback fired when item is resized,
+      stop: function (event, $element, widget) {}, // optional callback fired when item is finished resizing
+    },
+    draggable: {
+      enabled: false, // whether dragging items is supported
+      handle: ".my-class", // optional selector for drag handle
+      start: function (event, $element, widget) {
+        console.log("dragable");
+      }, // optional callback fired when drag is started,
+      drag: function (event, $element, widget) {
+        console.log("dragable");
+      }, // optional callback fired when item is moved,
+      stop: (event, $element, widget) => {
+        console.log("dragable");
+      },
+    },
   };
 
   $scope.move = function (note) {
